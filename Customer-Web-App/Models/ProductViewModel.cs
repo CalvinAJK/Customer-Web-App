2d9fbd765fad7689976f5be1042b19038827944b
CalvinAJK <calvinaujiakyn@gmail.com>
2024-01-16 06:42:29 +0000

    Minor fix

--- Customer-Web-App/Models/ProductViewModel.cs
+++ Customer-Web-App/Models/ProductViewModel.cs
@@ -7,6 +7,6 @@
         public string? Name{ get; set; }
         public string? Description { get; set; }
         public decimal? Price { get; set;}
-        public bool InStock { get; set;}
+        public Boolean InStock { get; set;}
     }
 }
