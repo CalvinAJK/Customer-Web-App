c41d01aec1a7b53fe8219991ed4967547541be59
CalvinAJK <calvinaujiakyn@gmail.com>
2024-01-16 01:38:52 +0000

    Fixing Stock not showing correctly due to different name  (InStock)

--- Customer-Web-App/Models/ProductViewModel.cs
+++ Customer-Web-App/Models/ProductViewModel.cs
@@ -7,6 +7,6 @@
         public string? Name{ get; set; }
         public string? Description { get; set; }
         public decimal? Price { get; set;}
-        public bool Stock { get; set;}
+        public bool InStock { get; set;}
     }
 }
